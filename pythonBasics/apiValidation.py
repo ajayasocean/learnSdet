e097dc98fc7892f00f721f2d176538148731d134
Ajay Sagar <ajay.sagar.cn@snapdeal.com>
2021-06-14 22:37:57 +0530

    learn api

---
+++ pythonBasics/apiValidation.py
@@ -0,0 +1,7 @@
+# request library
+# sample apis for practice on https://www.rahulshettyacademy.com/#/practice-project
+# Library api contract
+# https://drive.google.com/file/d/18FC3jDnsOol9zn3_KGSrjg35a4unpiSG/view
+import requests
+
+
