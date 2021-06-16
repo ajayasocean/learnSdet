8a6f0bf5f422d2ebe285be72ea6337131567a3e4
Ajay Sagar <ajay.sagar.cn@snapdeal.com>
2021-06-16 16:24:54 +0530

    improved get api test case

--- pythonBasics/apiValidation.py
+++ pythonBasics/apiValidation.py
@@ -5,11 +5,18 @@
 import requests
 # import json
 baseUrl = 'http://216.10.245.166/Library/GetBook.php'
-response1 = requests.get(baseUrl, params={'AuthorName': 'Tester'},)
+author = input('Please enter author name to search books: \n')
+response1 = requests.get(baseUrl, params={'AuthorName': author},)
 # print(response1.text)
 # dict_response1 = json.loads(response1.text)
 # print(dict_response1, '\n', type(dict_response1))
 response1Json = response1.json()  # json() method process response data received from request
-print(response1Json, '\n', type(response1Json))
-print(response1Json[0]['isbn'])  # printing 0th element from list i.e. dictionary that too for item isbn
-
+print('Below is the list of books:\n', response1Json)
+#  print(response1Json[0]['isbn'])  # printing 0th element from list and dictionary item with keys 'isbn'
+#  printing isbn key's value from response dynamically.
+lengthOfResponse1Json = len(response1Json)  # finding length of list in response
+print('Total number of books for the Author in response:', lengthOfResponse1Json)
+searchCriteria = input('Please enter search criteria among: book_name, isbn, aisle \n')
+for i in range(lengthOfResponse1Json):  # iterating through the list
+    dataResponse1 = response1Json[i]  # storing element in each iteration to a variable
+    print(searchCriteria, ':\t', dataResponse1[searchCriteria])  # printing as value of search criteria
