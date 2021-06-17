958dfb6548f6c9259ad2df534d9edd2bc7e8777f
Ajay Sagar <ajay.sagar.cn@snapdeal.com>
2021-06-17 23:00:39 +0530

    config from same project

--- pythonBasics/e2eApiTest.py
+++ pythonBasics/e2eApiTest.py
@@ -1,13 +1,18 @@
 # End to end automation flow of API calls using Python
 import requests
 from payload import *
+import configparser
 import json
+config = configparser.ConfigParser()
+config.read('globalProperties.ini')
+print(config.sections())
+baseUrl = config['api']['endpoint']
+print(baseUrl)
 # Add a new book
-baseUrl = 'http://216.10.245.166'
 pathAdd = '/Library/Addbook.php'
 finalUrlAdd = baseUrl+pathAdd
 head1 = {'Content-Type': 'application/json'}
-postResponse2 = requests.post(finalUrlAdd, json=addBookPayload('bill'), headers=head1,)
+postResponse2 = requests.post(finalUrlAdd, json=addBookPayload('mill'), headers=head1,)
 assert postResponse2.status_code == 200
 postResponse2Json = postResponse2.json()
 print('Book successfully added', postResponse2Json)
