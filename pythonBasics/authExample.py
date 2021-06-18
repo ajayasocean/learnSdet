bd3061909309e4572d0d8c84ae2a8775a352d10b
Ajay Sagar <ajay.sagar.cn@snapdeal.com>
2021-06-18 21:06:18 +0530

    removed access token

--- pythonBasics/authExample.py
+++ pythonBasics/authExample.py
@@ -8,26 +8,24 @@ gitAccessUrl = getconfig()['api']['gitHubUrl']
 print(gitAccessUrl)
 userName = getconfig()['gitHubCredentials']['userName']
 # get password by user, not required as basic auth deprecated on github since 5may 2021
-# def get_password():
-#     password = input('Please enter GitHub password:\n')
-#     return password
 # session manager
 with requests.Session() as sessionManager:
     HeadAccept = ApiResources.HeadAccept
     sessionManager.headers.update(HeadAccept)
-    HeadAuthorize = ApiResources.HeadAuthorize
-    sessionManager.headers.update(HeadAuthorize)
-# requesting git hub with authorization header
-# responseGitAuth = requests.get(gitAccessUrl+'/user', headers=head2)
-responseGitAuth = sessionManager.get(gitAccessUrl+'/user')
-print(responseGitAuth.status_code)
-print(responseGitAuth.json())
+    token = input('Please enter GitHub access token:\n')
+    resourceObj = ApiResources(token)
+    head_authorize = resourceObj.get_auth_token()
+    sessionManager.headers.update(head_authorize)
+    # requesting git hub with authorization header
+    responseGitAuth = sessionManager.get(gitAccessUrl+'/user')
+    print(responseGitAuth.status_code)
+    print(responseGitAuth.json())
 
-# accessing octokit org's repo
-pathOrgRepos = ApiResources.gitHubRepo  # preparing base url using configurations.py
-repoUrl = gitAccessUrl+pathOrgRepos  # making final url by adding base url and resource path from resources.py
-print(repoUrl)
-responseUserRepos = sessionManager.get(repoUrl)
-print(responseUserRepos.status_code)
-# print(responseUserRepos.json())
+    # accessing octokit org's repo
+    pathOrgRepos = ApiResources.gitHubRepo  # preparing base url using configurations.py
+    repoUrl = gitAccessUrl+pathOrgRepos  # making final url by adding base url and resource path from resources.py
+    print(repoUrl)
+    responseUserRepos = sessionManager.get(repoUrl)
+    print(responseUserRepos.status_code)
+    # print(responseUserRepos.json())
 
--- pythonBasics/resources.py
+++ pythonBasics/resources.py
@@ -4,5 +4,12 @@ class ApiResources:
     delBook = '/Library/DeleteBook.php'
     getBookByAuthorName = '/Library/GetBook.php'
     gitHubRepo = '/orgs/octokit/repos'
-    HeadAuthorize = {'Authorization': 'Bearer {}'.format('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')}
     HeadAccept = {'Accept': 'application/vnd.github.v3+json'}
+    # get access token from user at run time
+
+    def __init__(self, token):  # default constructor
+        self.head_authorize = {'Authorization': 'Bearer {}'.format(token)}
+
+    def get_auth_token(self):
+        return self.head_authorize
+
