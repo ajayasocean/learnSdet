4da4c631e808bffff48de2fa131fc779a0c07821
Ajay Sagar <ajay.sagar.cn@snapdeal.com>
2021-06-18 11:39:27 +0530

    session manager

--- pythonBasics/authExample.py
+++ pythonBasics/authExample.py
@@ -1,16 +1,35 @@
 # Authenticating API's using Python Automation auth method- Example
 # auth = ('user', 'password')
-from configurations import *
 import requests
+from configurations import *
+from resources import *
 
-url = getconfig()['api']['gitHubUrl']
+gitAccessUrl = getconfig()['api']['gitHubUrl']
+print(gitAccessUrl)
 userName = getconfig()['gitHubCredentials']['userName']
+# get password by user
 
 
-def getPassword():
+def get_password():
     password = input('Please enter GitHub password:\n')
     return password
 
 
-responseGit = requests.get(url, auth=(userName, getPassword()))
-print(responseGit.status_code)
+# session manager
+sessionManager = requests.session()
+sessionManager.auth = auth = (userName, get_password())
+head2 = ApiResources.acceptHeader
+# requesting git hub with username and password for access
+responseGitAuth = requests.get(gitAccessUrl+'/user', headers=head2, auth=(userName, get_password()))
+# responseGitAuth = sessionManager.get(gitAccessUrl+'/user', headers=head2)
+print(responseGitAuth.status_code)
+# print(responseGitAuth.text)
+
+# accessing an org repo
+pathOrgRepos = ApiResources.gitHubRepo  # preparing base url using configurations.py
+repoUrl = gitAccessUrl+pathOrgRepos  # making final url by adding base url and resource path from resources.py
+print(repoUrl)
+responseUserRepos = requests.get(repoUrl, headers=head2)
+print(responseUserRepos.status_code)
+# print(responseUserRepos.json())
+
--- pythonBasics/resources.py
+++ pythonBasics/resources.py
@@ -3,3 +3,5 @@ class ApiResources:
     addBook = '/Library/Addbook.php'
     delBook = '/Library/DeleteBook.php'
     getBookByAuthorName = '/Library/GetBook.php'
+    gitHubRepo = '/orgs/octokit/repos'
+    acceptHeader = {'Accept': 'application/vnd.github.v3+json'}
