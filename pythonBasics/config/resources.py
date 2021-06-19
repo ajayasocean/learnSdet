8f75c7f74c7f0f621d7c7d1f66652ca961e9fdfd
Ajay Sagar <ajay.sagar.cn@snapdeal.com>
2021-06-19 20:39:48 +0530

    api misleniuos

--- pythonBasics/config/resources.py
+++ pythonBasics/config/resources.py
@@ -5,6 +5,11 @@ class ApiResources:
     getBookByAuthorName = '/Library/GetBook.php'
     gitHubRepo = '/orgs/octokit/repos'
     HeadAccept = {'Accept': 'application/vnd.github.v3+json'}
+    head_accept = {'Accept': 'application/json'}
+    http_bin_cookies = '/cookies'
+    cookie = {'visit_month': 'May'}
+    pet_store_pet = '/pet'
+    pet_store_upload = '/uploadImage'
     # get access token from user at run time
 
     def __init__(self, token):  # default constructor
