193e88eb23dfe7487fe742dedee5fa5bf07aae67
Ajay Sagar <ajay.sagar.cn@snapdeal.com>
2021-06-20 21:42:34 +0530

    mysql connector

--- pythonBasics/config/configurations.py
+++ pythonBasics/config/configurations.py
@@ -1,8 +1,28 @@
 # configurations here
 import configparser
-
+import mysql.connector
+from mysql.connector import Error
 
 def get_config():
     config = configparser.ConfigParser()
     config.read('config/globalProperties.ini')
     return config
+
+
+connect_config = {'host': get_config()['SQL']['host'],
+                  'database': get_config()['SQL']['database_name'],
+                  'user': get_config()['SQL']['username'],
+                  'password': get_config()['SQL']['password'],
+                  }
+
+
+def get_connection():
+    # setting up a my sql connection with db
+    try:
+        connection = mysql.connector.connect(**connect_config)
+        if connection.is_connected():
+            print('connected successfully\n')
+            return connection
+    except Error as err:
+        print(err)
+
