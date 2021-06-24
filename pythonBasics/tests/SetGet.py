24eead98a21072955aa5173f8fc71b22b3b601f4
Ajay Sagar <ajay.sagar.cn@snapdeal.com>
2021-06-24 21:10:29 +0530

    misslenious

---
+++ pythonBasics/tests/SetGet.py
@@ -0,0 +1,12 @@
+"""
+getter setter : property() function
+A property object has three methods, getter(), setter(), and delete()
+property() function in Python has four arguments property(fget, fset, fdel, doc)
+fget is a function for retrieving an attribute value.
+fset is a function for setting an attribute value.
+fdel is a function for deleting an attribute value.
+doc creates a docstring for attribute.
+A property object has three methods, getter(), setter(), and delete()
+to specify fget, fset and fdel individually.
+"""
+
--- pythonBasics/tests/test.py
+++ pythonBasics/tests/test.py
@@ -1,37 +1,31 @@
-# Define a class where in you have two member variables num1 and num2
-# two functions for sum and product. constructor
-# getter setter
-
-
-# taking input from user and since this program is for sum and product, i'll eval them
-a = eval(input("Please first number\n"))
-b = eval(input("Please first second\n"))
+"""
+Define a class where in you have two member variables num1 and num2
+two functions for sum and product. constructor
+"""
 
 
 # declaring a class here
 class SumProd:
-    c = 3  # class variable
-
-    def __init__(self):  # constructor
-        num1 = a  # instance variables
-        num2 = b  # instance variables
+    def __init__(self, a, b):  # constructor
+        self._num1 = a
+        self._num2 = b
 
     # method to calculate total (sum)
     def get_total_of_nums(self):
-        total = self.num1 + self.num2
-        return total
+        return self._num1 + self._num2
 
     # method to calculate product (multiply)
     def get_product_of_nums(self):
-        product = self.num1 * self.num2
-        return product
+        return self._num1 * self._num2
 
 
-obj = SumProd()  # object instance
-obj.num1 = a  # assigning value to call variable
-obj.num2 = b  # assigning value to call variable
-total = obj.get_total_of_nums()  # method call for total
-product = obj.get_product_of_nums()  # method call for multiply
+# taking input from user and since this program is for sum and product, i'll eval them
+def driver():
+    a = eval(input("Please first number\n"))
+    b = eval(input("Please first second\n"))
+    obj = SumProd(a, b)  # object instance
+    print('sum of numbers', obj.get_total_of_nums())  # method call for total
+    print('product of numbers', obj.get_product_of_nums())  # method call for multiply
 
-print('sum of numbers', total)
-print('product of numbers', product)
+
+driver()
