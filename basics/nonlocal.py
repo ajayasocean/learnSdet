03ba4d5c1581d39a76a210c43ffd6711c381ca51
Ajay Sagar <ajay.sagar.cn@snapdeal.com>
2021-07-06 19:50:53 +0530

    basics

---
+++ basics/nonlocal.py
@@ -0,0 +1,11 @@
+# example nonlocal variable
+
+def outer():
+    x = 'local'
+    def inner():
+        nonlocal x
+        x = 'non-local'
+        print('inner: ', x)
+    inner()
+    print('outer: ', x)
+outer()
